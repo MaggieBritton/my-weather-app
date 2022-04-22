34b672a0f42dc8b8b8b37231dfe275537a96688f
Margaret Britton <margaret.ann.britton@gmail.com>
2022-04-22 13:59:34 -0500

    Home Screen Current Temp

--- Components/HomeComponent.js
+++ Components/HomeComponent.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 import MenuIcon from '@mui/icons-material/Menu';
+import WbSunnyOutlinedIcon from '@mui/icons-material/WbSunnyOutlined';
 
-export const DateLocation = () => {
+const DateLocation = () => {
   return (
     <View style={styles.headerContainer}>
         <View>
@@ -16,11 +17,45 @@ export const DateLocation = () => {
   )
 };
 
+const CurrentTemp = () => {
+    return (
+        <View>
+            <View style={styles.currentTempContainer}>
+                <Text style={styles.currentTemp}>58</Text>
+                <Text style={styles.degree}>&deg;F</Text>
+            </View>
+            <WbSunnyOutlinedIcon style={{fontSize: 100, alignSelf: 'center'}} />
+            <View style={styles.highLowContainer}>
+                <Text style={styles.highLowText}>High: 65&deg;F</Text>
+                <Text style={styles.highLowText}>Low: 44&deg;F</Text>
+            </View>
+        </View>
+    )
+};
+
+const Forecast = () => {
+    return (
+        <View style={styles.forecastContainer}>
+            <View style={styles.dailyForecastContainer}>
+                <Text>Thursday</Text>
+                <WbSunnyOutlinedIcon />
+                <View style={styles.forecastTempsContainer}>
+                    <Text style={{marginRight: 20}}>65</Text>
+                    <Text>38</Text>
+                </View>
+            </View>
+
+        </View>
+    )
+};
+
 
 const Home = () => {
   return (
     <View>
         <DateLocation/>
+        <CurrentTemp />
+        <Forecast />
     </View>
   )
 };
@@ -40,6 +75,39 @@ const styles = StyleSheet.create({
     location: {
         fontSize: 24,
         paddingTop: 10
+    },
+    currentTempContainer: {
+        flexDirection: 'row',
+        justifyContent: 'center',
+        paddingTop: 60,
+        paddingBottom: 25
+    },
+    currentTemp: {
+        fontSize: 72,
+        fontWeight: '500'
+    },
+    degree: {
+        fontSize: 24,
+        paddingTop: 10
+    },
+    highLowContainer: {
+        flex: 1,
+        flexDirection: 'row',
+        justifyContent: 'space-evenly',
+        paddingTop: 25,
+        
+    },
+    highLowText: {
+        fontSize: 16
+    },
+    dailyForecastContainer: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        marginBottom: 50
+    },
+    forecastTempsContainer: {
+        flexDirection: 'row',
     }
 });
 
