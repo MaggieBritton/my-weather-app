864caf34ed1c5a8e394a67a6d9059418b400577e
Margaret Britton <margaret.ann.britton@gmail.com>
2022-04-23 23:01:03 -0500

    Gradient Background and Attempt to use Google Fonts

--- App.js
+++ App.js
@@ -1,12 +1,28 @@
-import { StyleSheet, View } from 'react-native';
+import { StyleSheet, View, Text } from 'react-native';
 import HomeComponent from './Components/HomeComponent';
+import { LinearGradient } from 'expo-linear-gradient';
+import { useFonts, WorkSans_400Regular } from '@expo-google-fonts/work-sans';
+import AppLoading from 'expo-app-loading';
 
 export default function App() {
+
+  let [fontsLoaded, error] = useFonts({
+    WorkSans_400Regular
+  });
+
+  if (!fontsLoaded) {
+    return <AppLoading />
+  }
+
   return (
     <View style={styles.container}>
-      <View style={styles.homeContainer}>
+      <LinearGradient
+        colors={['#830165', '#5c0087']}
+        locations={[0.2, 0.5]}
+      >
+        <Text style={{fontFamily: 'WorkSans_400Regular', color: '#fff', fontSize: 40}}>This is a font test.</Text>
         <HomeComponent />
-      </View>
+      </LinearGradient>
     </View>
   );
 }
@@ -14,9 +30,6 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#5c0087'
-  },
-  homeContainer: {
-    flex: 1,
+    backgroundColor: '#5c0187'
   }
 })
